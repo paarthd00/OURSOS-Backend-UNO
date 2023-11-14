f61a37b297fa242bef9d8c55b3309e5706eab863
paarthd00 <paarthcode@gmail.com>
2023-11-14 02:28:31 -0800

    Cache Earthquake and fire data

--- api/earthquakes.go
+++ api/earthquakes.go
@@ -1,38 +1,59 @@
 package api
 
 import (
+	"context"
 	"net/http"
 	"time"
 
 	"encoding/json"
 
 	"github.com/labstack/echo/v4"
+	"oursos.com/packages/redis"
 	"oursos.com/packages/util"
 )
 
 func GetEarthQuakes(c echo.Context) error {
-	currentTime := time.Now().UTC()
 
-	yesterday := currentTime.AddDate(0, 0, -1)
+	client := redis.Client()
+	ctx := context.Background()
 
-	yesterdayDate := yesterday.Format("2006-01-02")
-	todaysDate := currentTime.Format("2006-01-02")
-
-	apiURL := "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" + yesterdayDate + "&endtime=" + todaysDate
-
-	req, _ := http.NewRequest("GET", apiURL, nil)
-
-	res, err := http.DefaultClient.Do(req)
+	exists, err := client.Exists(ctx, "earthquakes").Result()
 	util.CheckError(err)
-	defer res.Body.Close()
+	var earthquakeJSON interface{}
 
-	if res.StatusCode != http.StatusOK {
-		return c.JSON(res.StatusCode, map[string]string{"error": "Failed to retrieve earthquake data"})
+	if exists == 1 {
+		earthquakes_json := client.Get(ctx, "earthquakes").Val()
+		err = json.Unmarshal([]byte(earthquakes_json), &earthquakeJSON)
+		util.CheckError(err)
+		println("redis")
+	} else {
+		currentTime := time.Now().UTC()
+
+		yesterday := currentTime.AddDate(0, 0, -1)
+
+		yesterdayDate := yesterday.Format("2006-01-02")
+		todaysDate := currentTime.Format("2006-01-02")
+
+		apiURL := "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" + yesterdayDate + "&endtime=" + todaysDate
+
+		req, _ := http.NewRequest("GET", apiURL, nil)
+
+		res, err := http.DefaultClient.Do(req)
+		util.CheckError(err)
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusOK {
+			return c.JSON(res.StatusCode, map[string]string{"error": "Failed to retrieve earthquake data"})
+		}
+		// Read the response body
+		json.NewDecoder(res.Body).Decode(&earthquakeJSON)
+
+		util.CheckError(err)
+		earthquakes_json, err := json.Marshal(earthquakeJSON)
+		util.CheckError(err)
+		rediserr := client.Set(ctx, "earthquakes", earthquakes_json, 0).Err()
+		util.CheckError(rediserr)
 	}
-	var bodyJSON interface{}
-	// Read the response body
-	json.NewDecoder(res.Body).Decode(&bodyJSON)
 
-	util.CheckError(err)
-	return c.JSON(http.StatusOK, bodyJSON)
+	return c.JSON(http.StatusOK, earthquakeJSON)
 }
--- api/forest_fire.go
+++ api/forest_fire.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"context"
 	"encoding/csv"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +16,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"oursos.com/packages/redis"
 	"oursos.com/packages/util"
 )
 
@@ -41,55 +44,72 @@ func GetForestFireData(c echo.Context) error {
 
 	util.CheckError(err)
 	currentTime := time.Now().UTC()
+	client := redis.Client()
+	ctx := context.Background()
 
-	todaysDate := currentTime.AddDate(0, 0, -1).Format("2006-01-02")
-	key := os.Getenv("NASA_FIRE_KEY")
-	apiURL := "https://firms.modaps.eosdis.nasa.gov/api/country/csv/" + key + "/VIIRS_SNPP_NRT/CAN/1/" + todaysDate
-	response, err := http.Get(apiURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-
-	// Create a CSV reader to read the response body
-	reader := csv.NewReader(response.Body)
-
-	// Read and parse the CSV records
-	var records [][]string
-	for {
-		record, err := reader.Read()
-		if err != nil {
-			break
-		}
-		// Replace stray double-quote characters in each field
-		for i := range record {
-			record[i] = strings.Replace(record[i], `"`, ``, -1)
-		}
-		records = append(records, record)
-	}
+	exists, err := client.Exists(ctx, "fires").Result()
+	util.CheckError(err)
 
 	var fireDataList []FireData
 
-	// Iterate through the CSV records and populate the FireData struct
-	for _, record := range records[1:] { // Skip the header row
-		fireData := FireData{
-			CountryID:  record[0],
-			Latitude:   record[1],
-			Longitude:  record[2],
-			BrightTI4:  record[3],
-			Scan:       record[4],
-			Track:      record[5],
-			AcqDate:    record[6],
-			AcqTime:    record[7],
-			Satellite:  record[8],
-			Instrument: record[9],
-			Confidence: record[10],
-			Version:    record[11],
-			BrightTI5:  record[12],
-			FRP:        record[13],
-			DayNight:   record[14],
+	if exists == 1 {
+		fireDataList_json := client.Get(ctx, "fires").Val()
+		err = json.Unmarshal([]byte(fireDataList_json), &fireDataList)
+		util.CheckError(err)
+		println("redis")
+	} else {
+
+		todaysDate := currentTime.AddDate(0, 0, -1).Format("2006-01-02")
+		key := os.Getenv("NASA_FIRE_KEY")
+		apiURL := "https://firms.modaps.eosdis.nasa.gov/api/country/csv/" + key + "/VIIRS_SNPP_NRT/CAN/1/" + todaysDate
+		response, err := http.Get(apiURL)
+		if err != nil {
+			log.Fatal(err)
 		}
-		fireDataList = append(fireDataList, fireData)
+		defer response.Body.Close()
+
+		// Create a CSV reader to read the response body
+		reader := csv.NewReader(response.Body)
+
+		// Read and parse the CSV records
+		var records [][]string
+		for {
+			record, err := reader.Read()
+			if err != nil {
+				break
+			}
+			// Replace stray double-quote characters in each field
+			for i := range record {
+				record[i] = strings.Replace(record[i], `"`, ``, -1)
+			}
+			records = append(records, record)
+		}
+
+		// Iterate through the CSV records and populate the FireData struct
+		for _, record := range records[1:] { // Skip the header row
+			fireData := FireData{
+				CountryID:  record[0],
+				Latitude:   record[1],
+				Longitude:  record[2],
+				BrightTI4:  record[3],
+				Scan:       record[4],
+				Track:      record[5],
+				AcqDate:    record[6],
+				AcqTime:    record[7],
+				Satellite:  record[8],
+				Instrument: record[9],
+				Confidence: record[10],
+				Version:    record[11],
+				BrightTI5:  record[12],
+				FRP:        record[13],
+				DayNight:   record[14],
+			}
+			fireDataList = append(fireDataList, fireData)
+		}
+		fireDataList_json, err := json.Marshal(fireDataList)
+		util.CheckError(err)
+		rediserr := client.Set(ctx, "fires", fireDataList_json, 0).Err()
+		util.CheckError(rediserr)
 	}
 
 	return c.JSON(http.StatusOK, fireDataList)
