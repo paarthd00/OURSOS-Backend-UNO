be8760eaf49678564b391d97b33d4754167c7471
paarthd00 <paarthcode@gmail.com>
2023-11-22 20:23:16 -0800

    Turn on redis for alerts

--- alerts/get_all_alerts.go
+++ alerts/get_all_alerts.go
@@ -1,12 +1,15 @@
 package alerts
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
 	"oursos.com/packages/db"
+	"oursos.com/packages/redis"
 
 	"oursos.com/packages/util"
 )
@@ -28,16 +31,31 @@ func GetAllAlertsHandler(c echo.Context) error {
 	rows, err := db.Query("SELECT * FROM alerts")
 	util.CheckError(err)
 	defer rows.Close()
+	client := redis.Client()
+	ctx := context.Background()
 
+	exists, err := client.Exists(ctx, "alerts").Result()
 	util.CheckError(err)
 	var alerts []Alert
 
-	for rows.Next() {
-		var alert Alert
-		if err := rows.Scan(&alert.ID, &alert.Message, &alert.Category, &alert.Severity, &alert.Time, &alert.Latitude, &alert.Longitude, &alert.Radius); err != nil {
-			util.CheckError(err)
+	if exists == 1 {
+		alerts_json := client.Get(ctx, "alerts").Val()
+		err = json.Unmarshal([]byte(alerts_json), &alerts)
+		util.CheckError(err)
+		println("redis")
+	} else {
+		for rows.Next() {
+			var alert Alert
+			if err := rows.Scan(&alert.ID, &alert.Message, &alert.Category, &alert.Severity, &alert.Time, &alert.Latitude, &alert.Longitude, &alert.Radius); err != nil {
+				util.CheckError(err)
+			}
+			alerts = append(alerts, alert)
 		}
-		alerts = append(alerts, alert)
+		alerts_json, err := json.Marshal(alerts)
+		util.CheckError(err)
+		rediserr := client.Set(ctx, "alerts", alerts_json, 0).Err()
+		util.CheckError(rediserr)
 	}
+
 	return c.JSON(http.StatusOK, alerts)
 }
